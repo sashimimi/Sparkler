5cc746320f481ca71e81e7c747c580f12ab10762
sashimimi <mimikhlam@gmail.com>
2019-07-04 13:36:18 -0500

    Add project base

---
+++ src/main/java/io/github/sashimimi/sparkler/Sparkler.java
@@ -0,0 +1,23 @@
+package io.github.sashimimi.sparkler;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Sparkler extends JavaPlugin
+{
+	private static Sparkler instance;
+	
+	public void onEnable()
+	{
+		
+	}
+	
+	public void onDisable()
+	{
+		
+	}
+	
+	public static Sparkler getInstance()
+	{
+		return instance;
+	}
+}
