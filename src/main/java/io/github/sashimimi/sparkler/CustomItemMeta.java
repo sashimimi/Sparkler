7d37186731185949786f85e3ee0e7c33f2bf177b
sashimimi <mimikhlam@gmail.com>
2019-07-04 15:15:38 -0500

    Add custom fireworks method

--- src/main/java/io/github/sashimimi/sparkler/CustomItemMeta.java
+++ src/main/java/io/github/sashimimi/sparkler/CustomItemMeta.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 import org.bukkit.Color;
 import org.bukkit.DyeColor;
+import org.bukkit.FireworkEffect;
 import org.bukkit.Material;
 import org.bukkit.block.banner.Pattern;
 import org.bukkit.block.banner.PatternType;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BannerMeta;
+import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
 
 public class CustomItemMeta 
@@ -36,6 +38,21 @@ public class CustomItemMeta
 		
 		meta.setColor(color);
 		meta.addEnchant(Enchantment.PROTECTION_EXPLOSIONS, 3, false);
+		
+		item.setItemMeta(meta);
+		
+		return item;
+	}
+	
+	public static ItemStack customFireworks(List<String> colors, FireworkEffect.Type type)
+	{
+		ItemStack item = new ItemStack(Material.FIREWORK_ROCKET, 64);
+		FireworkMeta meta = (FireworkMeta) item.getItemMeta();
+		FireworkEffect.Builder builder = FireworkEffect.builder();
+		
+		builder.withColor(colors).withTrail().with(type);
+		meta.addEffect(builder.build());
+		meta.setPower(2);
 		
 		item.setItemMeta(meta);
 		
